46d759b8daa200b36dc8aee5647cb75c41b9c981
pCarbonero <pablo.carbonero@iesnervion.es>
2025-02-14 08:27:00 +0100

    [upd] se me olvido :(

--- SelniumTest/ejercicio02/ejercicio02/views/form/form.py
+++ SelniumTest/ejercicio02/ejercicio02/views/form/form.py
@@ -2,21 +2,24 @@ import reflex as rx
 
 def form() -> rx.Component:
     return rx.vstack(
-        rx.vstack(
-            rx.hstack(
+        rx.form(
+            rx.vstack(
+                rx.hstack(
                 rx.text("Nombre: "),
-                rx.input(
-                    placeholder="Nombre",
-                    type="text",
-                    id="campoNombre"
+                    rx.input(
+                        placeholder="Nombre",
+                        type="text",
+                        id="campoNombre",
+                        max_length = 50
+                    ),
                 ),
-            ),
             rx.hstack(
                 rx.text("Apellidos: "),
                 rx.input(
                     placeholder="Apellidos",
                     type="text",
-                    id="campoApellidos"
+                    id="campoApellidos",
+                    max_length = 50
                 ),
             ),
             rx.hstack(
@@ -26,6 +29,25 @@ def form() -> rx.Component:
                     direction="row",
                     id="sexRadio"
                 )
-            )
-        )
+            ),
+            rx.hstack(
+                rx.text("Correo: "),
+                rx.input(
+                    placeholder="Correo Electrónico",
+                    type="email",
+                    id="mailInput"
+                ),
+            ),
+            rx.hstack(
+                rx.checkbox(default_checked=True, id="checkInformacion"),
+                rx.text("Deseo recibir información sobre novedades y ofertas", id="textInformacion") 
+            ),
+            rx.hstack(
+                rx.checkbox(id="checkCondiciones"),
+                rx.text("Declaro haber leido y aceptar las condiciones generales del programa y la normativa sobre protección de datos", id="textCondiciones") 
+            ),
+            rx.button("Submit", type="submit"),
+            ), 
+            reset_on_submit=True,
+       )       
     )
